1e601a80d1c5b4d8848d7f1a6bbef3a09a1ef675
June Rhodes <jrhodes@redpointgames.com.au>
2017-04-30 16:13:22 +1000

    Allow host applications to be automatically launched when debugging mobile apps

--- Protobuild.IDE.VisualStudio/CrossPlatformPackage.cs
+++ Protobuild.IDE.VisualStudio/CrossPlatformPackage.cs
@@ -1,75 +1,216 @@
-﻿//------------------------------------------------------------------------------
-// <copyright file="CrossPlatformPackage.cs" company="Company">
-//     Copyright (c) Company.  All rights reserved.
-// </copyright>
-//------------------------------------------------------------------------------
-
-using System;
-using System.ComponentModel.Design;
+﻿using System;
 using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
 using System.Runtime.InteropServices;
 using Microsoft.VisualStudio;
-using Microsoft.VisualStudio.OLE.Interop;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
-using Microsoft.Win32;
+using EnvDTE;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+using System.Management;
+using Process = System.Diagnostics.Process;
 
 namespace Protobuild.IDE.VisualStudio
 {
-    /// <summary>
-    /// This is the class that implements the package exposed by this assembly.
-    /// </summary>
-    /// <remarks>
-    /// <para>
-    /// The minimum requirement for a class to be considered a valid package for Visual Studio
-    /// is to implement the IVsPackage interface and register itself with the shell.
-    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
-    /// to do it: it derives from the Package class that provides the implementation of the
-    /// IVsPackage interface and uses the registration attributes defined in the framework to
-    /// register itself and its components with the shell. These attributes tell the pkgdef creation
-    /// utility what data to put into .pkgdef file.
-    /// </para>
-    /// <para>
-    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
-    /// </para>
-    /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true)]
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
-    [Guid(CrossPlatformPackage.PackageGuidString)]
-    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    [Guid("e3556644-2e00-4616-8507-15e3cd889588")]
     [ProvideAutoLoad(UIContextGuids.NoSolution)]
     [ProvideAutoLoad(UIContextGuids.SolutionExists)]
-    public sealed class CrossPlatformPackage : Package
+    public sealed class ProtobuildPackage : Package, IVsDebuggerEvents, IVsSolutionEvents
     {
-        /// <summary>
-        /// CrossPlatformPackage GUID string.
-        /// </summary>
-        public const string PackageGuidString = "e3556644-2e00-4616-8507-15e3cd889588";
+        private IVsSolution2 _solution;
+        private uint _solutionEventsCookie;
+        private IVsDebugger _debugger;
+        private uint _debuggerEventsCookie;
+        private DTE _dte;
+        private IVsSolutionBuildManager _solutionBuildManager;
+        private List<System.Diagnostics.Process> _runningProcesses = new List<System.Diagnostics.Process>();
+        private bool _isDebugging;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="CrossPlatformPackage"/> class.
-        /// </summary>
-        public CrossPlatformPackage()
-        {
-            // Inside this method you can place any initialization code that does not require
-            // any Visual Studio service because at this point the package object is created but
-            // not sited yet inside Visual Studio environment. The place to do all the other
-            // initialization is the Initialize method.
-        }
-
-        #region Package Members
-
-        /// <summary>
-        /// Initialization of the package; this method is called right after the package is sited, so this is the place
-        /// where you can put all the initialization code that rely on services provided by VisualStudio.
-        /// </summary>
         protected override void Initialize()
         {
-            base.Initialize();
+            _dte = GetService(typeof(DTE)) as DTE;
+
+            _solution = ServiceProvider.GlobalProvider.GetService(typeof(SVsSolution)) as IVsSolution2;
+            if (_solution != null)
+            {
+                _solution.AdviseSolutionEvents(this, out _solutionEventsCookie);
+            }
+
+            _debugger = ServiceProvider.GlobalProvider.GetService(typeof(SVsShellDebugger)) as IVsDebugger;
+            if (_debugger != null)
+            {
+                _debugger.AdviseDebuggerEvents(this, out _debuggerEventsCookie);
+            }
+
+            _solutionBuildManager = ServiceProvider.GlobalProvider.GetService(typeof(IVsSolutionBuildManager)) as IVsSolutionBuildManager;
         }
 
-        #endregion
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+
+            if (_solution != null)
+            {
+                _solution.UnadviseSolutionEvents(_solutionEventsCookie);
+            }
+            if (_debugger != null)
+            {
+                _debugger.UnadviseDebuggerEvents(_debuggerEventsCookie);
+            }
+        }
+
+        public int OnAfterOpenProject(IVsHierarchy pHierarchy, int fAdded)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnQueryCloseProject(IVsHierarchy pHierarchy, int fRemoving, ref int pfCancel)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnBeforeCloseProject(IVsHierarchy pHierarchy, int fRemoved)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnAfterLoadProject(IVsHierarchy pStubHierarchy, IVsHierarchy pRealHierarchy)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnQueryUnloadProject(IVsHierarchy pRealHierarchy, ref int pfCancel)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnBeforeUnloadProject(IVsHierarchy pRealHierarchy, IVsHierarchy pStubHierarchy)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnAfterOpenSolution(object pUnkReserved, int fNewSolution)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnQueryCloseSolution(object pUnkReserved, ref int pfCancel)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnBeforeCloseSolution(object pUnkReserved)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnAfterCloseSolution(object pUnkReserved)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnModeChange(DBGMODE dbgmodeNew)
+        {
+            if (dbgmodeNew == DBGMODE.DBGMODE_Run)
+            {
+                if (_isDebugging)
+                {
+                    return VSConstants.S_OK;
+                }
+
+                _isDebugging = true;
+
+                var startupProjects = _dte.Solution.SolutionBuild.StartupProjects as object[];
+                if (startupProjects == null)
+                {
+                    return VSConstants.S_OK;
+                }
+
+                var startupProjectPaths = startupProjects.Cast<string>().ToArray();
+                foreach (var projectUniqueName in startupProjectPaths)
+                {
+                    IVsHierarchy hierarchy;
+                    _solution.GetProjectOfUniqueName(projectUniqueName, out hierarchy);
+
+                    var activeConfigurations = new IVsProjectCfg[1];
+                    if (_solutionBuildManager.FindActiveProjectCfg(IntPtr.Zero, IntPtr.Zero, hierarchy, activeConfigurations) == VSConstants.S_OK)
+                    {
+                        string configurationName;
+                        if (activeConfigurations[0].get_CanonicalName(out configurationName) == VSConstants.S_OK)
+                        {
+                            var buildPropertyStorage = hierarchy as IVsBuildPropertyStorage;
+                            if (buildPropertyStorage != null)
+                            {
+                                string hostAppExecutable;
+                                string hostAppArguments;
+                                string hostAppWorkingDirectory;
+                                if (buildPropertyStorage.GetPropertyValue("ProtobuildLaunchHostAppExecutable", configurationName, (uint)_PersistStorageType.PST_PROJECT_FILE, out hostAppExecutable) == VSConstants.S_OK &&
+                                    buildPropertyStorage.GetPropertyValue("ProtobuildLaunchHostAppArguments", configurationName, (uint)_PersistStorageType.PST_PROJECT_FILE, out hostAppArguments) == VSConstants.S_OK &&
+                                    buildPropertyStorage.GetPropertyValue("ProtobuildLaunchHostAppWorkingDirectory", configurationName, (uint)_PersistStorageType.PST_PROJECT_FILE, out hostAppWorkingDirectory) == VSConstants.S_OK)
+                                {
+                                    string launchPath = null;
+                                    if (Path.IsPathRooted(hostAppExecutable))
+                                    {
+                                        launchPath = hostAppExecutable;
+                                    }
+                                    else
+                                    {
+                                        launchPath = Path.Combine(new FileInfo(_dte.Solution.FileName).DirectoryName, hostAppExecutable);
+                                    }
+
+                                    try
+                                    {
+                                        var processStartInfo = new ProcessStartInfo(launchPath, hostAppArguments);
+                                        processStartInfo.WorkingDirectory = hostAppWorkingDirectory;
+                                        processStartInfo.UseShellExecute = false;
+                                        _runningProcesses.Add(Process.Start(processStartInfo));
+                                    }
+                                    catch
+                                    {
+                                        // Ignore
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            else if (dbgmodeNew == DBGMODE.DBGMODE_Design)
+            {
+                _isDebugging = false;
+
+                // Stop host processes.
+                foreach (var process in _runningProcesses)
+                {
+                    KillProcessAndChildren(process.Id);
+                }
+                _runningProcesses.Clear();
+            }
+
+            return VSConstants.S_OK;
+        }
+
+        private static void KillProcessAndChildren(int pid)
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher
+              ("Select * From Win32_Process Where ParentProcessID=" + pid);
+            ManagementObjectCollection moc = searcher.Get();
+            foreach (ManagementObject mo in moc)
+            {
+                KillProcessAndChildren(Convert.ToInt32(mo["ProcessID"]));
+            }
+            try
+            {
+                Process proc = Process.GetProcessById(pid);
+                proc.Kill();
+            }
+            catch (Exception)
+            {
+                // Process already exited.
+            }
+        }
     }
 }
